071c2db311192f3f54ce6eb17b23e02558cfb875
sloveniatechsolution-hue <sloveniatechsolution@gmail.com>
2026-01-11 23:49:49 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+
 export default function AboutPage() {
   return (
     <div className="w-full">
